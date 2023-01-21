e27094329641392808adce802f4b7adb002797fe
Joel Onwuanaku <joelebuka@gmail.com>
2023-01-21 16:53:00 +0100

    Fixed contact issue

--- src/components/global/layouts/Header.js
+++ src/components/global/layouts/Header.js
@@ -1,14 +1,16 @@
 export default function Header({ header, children }) {
   return (
-    <header className="header container">
-      <div className="header__title">
-        <h4>{header}</h4>
-      </div>
-      <div className="header__image">
-        <svg>
-          <use href="/images/sprite.svg/#icon-about" />
-        </svg>
-      </div>
-    </header>
+    <div className="container">
+      <header className="header ">
+        <div className="header__title">
+          <h4>{header}</h4>
+        </div>
+        <div className="header__image">
+          <svg>
+            <use href="/images/sprite.svg/#icon-about" />
+          </svg>
+        </div>
+      </header>
+    </div>
   );
 }
