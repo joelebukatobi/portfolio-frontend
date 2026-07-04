9a2b1d4ab37a88fd2975624f74f9e845836b851b
Joel Onwuanaku <joelebuka@gmail.com>
2026-07-04 11:28:48 -0400

    refactor: render brand social icons as native img elements
    Use standalone SVG files for footer and share brand marks so colors come from the assets instead of CSS fill overrides.

--- src/app/templates/partials/share.js
+++ src/app/templates/partials/share.js
@@ -1,27 +1,33 @@
+export function shareIconLinks({ slug, url } = {}) {
+  const shareUrl = url || `https://www.joelebukatobi.dev/blog/${slug}`;
+
+  return `
+    <li>
+      <a href="https://www.facebook.com/sharer.php?u=${shareUrl}" target="_blank" rel="noopener noreferrer">
+        <img class="brand-icon" src="/images/icons/facebook.svg" alt="" aria-hidden="true" />
+      </a>
+    </li>
+    <li>
+      <a href="https://twitter.com/intent/tweet?url=${shareUrl}" target="_blank" rel="noopener noreferrer">
+        <img class="brand-icon" src="/images/icons/twitter.svg" alt="" aria-hidden="true" />
+      </a>
+    </li>
+    <li>
+      <a href="https://www.linkedin.com/shareArticle?url=${shareUrl}" target="_blank" rel="noopener noreferrer">
+        <img class="brand-icon" src="/images/icons/linkedin.svg" alt="" aria-hidden="true" />
+      </a>
+    </li>`;
+}
+
 export function share({ slug, className = '' } = {}) {
   const extraClass = className ? ` ${className}` : '';
-  const url = `https://www.joelebukatobi.dev/blog/${slug}`;
 
   return `
 <div class="blog__share${extraClass}">
   <h6>Share</h6>
   <hr />
   <ul>
-    <li>
-      <a href="https://www.facebook.com/sharer.php?u=${url}" target="_blank">
-        <svg><use href="/images/sprite.svg#icon-facebook" /></svg>
-      </a>
-    </li>
-    <li>
-      <a href="https://twitter.com/intent/tweet?url=${url}" target="_blank">
-        <svg><use href="/images/sprite.svg#icon-twitter" /></svg>
-      </a>
-    </li>
-    <li>
-      <a href="https://www.linkedin.com/shareArticle?url=${url}" target="_blank">
-        <svg><use href="/images/sprite.svg#icon-linkedin" /></svg>
-      </a>
-    </li>
+    ${shareIconLinks({ slug })}
   </ul>
 </div>`;
 }
--- src/app/templates/partials/socials.js
+++ src/app/templates/partials/socials.js
@@ -3,19 +3,19 @@ export function socials({ className = 'copyright__list' } = {}) {
   <ul class="${className}">
     <li>
       <a href="https://www.linkedin.com/in/joelebukatobi/">
-        <svg><use href="/images/sprite.svg#icon-linkedin" /></svg>
+        <img class="brand-icon" src="/images/icons/linkedin.svg" alt="" aria-hidden="true" />
         <p>LinkedIn</p>
       </a>
     </li>
     <li>
       <a href="https://github.com/joelebukatobi">
-        <svg><use href="/images/sprite.svg#icon-github" /></svg>
+        <img class="brand-icon" src="/images/icons/github.svg" alt="" aria-hidden="true" />
         <p>Github</p>
       </a>
     </li>
     <li>
       <a href="mailto:joelebuka@gmail.com">
-        <svg><use href="/images/sprite.svg#icon-email" /></svg>
+        <img class="brand-icon" src="/images/icons/email.svg" alt="" aria-hidden="true" />
         <p>Email</p>
       </a>
     </li>
